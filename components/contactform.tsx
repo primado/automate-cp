f7d6ec1e7a7aa628af321c2359f5e0d4a6eb701c
primado <primadoprincepa@gmail.com>
2024-04-29 10:42:28 +0000

    remove completed code

--- components/contactform.tsx
+++ components/contactform.tsx
@@ -1,37 +1,16 @@
 'use client'
-import { useForm } from "react-hook-form"
-import axios from "axios"
-import { useMutation } from "@tanstack/react-query"
-import Image from "next/image"
-import CarouselComponent from "./carousel"
-import { ErrorMessage } from "@hookform/error-message"
+
 
 import img_one from "../public/nathan-dumlao-drov8-6RLUE-unsplash.jpg"
+import CarouselComponent from "./carousel"
 
-type ContactType = {
-    first_name: string,
-    last_name: string,
-    phone_number: string,
-    email_address: string,
-    message: string
-}
+
+
+
+const api_url = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export default function ContactForm() {
 
-    const {register, handleSubmit, formState: { errors}} = useForm<ContactType>({
-        criteriaMode: 'all',
-        defaultValues: {
-            first_name: "",
-            last_name: "",
-            email_address: "",
-            phone_number: "",
-            message: ""
-        }
-    })
-        
-    const onSubmit = async (data: ContactType) => {
-        console.log("Contact form data => ", data)
-    }
     
     
     return (
@@ -40,69 +19,31 @@ export default function ContactForm() {
                 <div className="flex flex-col justify-center items-start px-[10%]">
                     <div className="flex flex-col gap-y-4 ">
                         <div className="flex flex-col gap-y-1 ">
-                            <p className="text-dark-blue font-medium text-lg">Contact us</p>
-                            <h1 className="text-2xl font-bold text-dark">Get in touch</h1>
+                            <p className="text-dark-blue font-semibold text-lg">Contact us</p>
+                            <h1 className="text-3xl  font-bold text-dark">Get in touch</h1>
                            
                         </div>
                         <p className="text-lg font-medium text-gray-500">We&apos;d love to hear from you. Please fill this form</p>
                    </div>
-                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-5 mt-5">
+                    <form className="flex flex-col gap-y-5 mt-5">
                         <div className="flex flex-row gap-y-3 gap-x-8 ">
                             <div className="flex flex-col gap-y-2">
                                 <label htmlFor="fname">First Name</label>
                                 <input 
                                     type="text" 
-                                    {...register("first_name", {
-                                        required: {
-                                            value: true,
-                                            message: "First name is required"
-                                        },
-                                        maxLength: {
-                                            value: 15,
-                                            message: "Accepts a maximum of 150 characters"
-                                        }
-                                    })}
                                     placeholder="First name"
                                     className="focus:outline-nontext-base font-medium focus:outline-none ring-2 ring-gray-400 p-2 rounded-md focus:ring-2 focus:ring-dark-blue"
                                 />
-                                 <ErrorMessage
-                                   errors={errors}
-                                   name="first_name"
-                                   render={({ messages }) =>
-                                     messages &&
-                                     Object.entries(messages).map(([type, message]) => (
-                                       <p key={type} className="text-sm text-red-500 font-normal">{message}</p>
-                                     ))
-                                   }
-                                 />
+                                
                             </div>
                             <div className="flex flex-col gap-y-2">
                                 <label htmlFor="lname">Last Name</label>
                                 <input 
                                     type="text" 
-                                    {...register("last_name", {
-                                        required: {
-                                            value: true,
-                                            message: "Last name is required"
-                                        },
-                                        maxLength: {
-                                            value: 15,
-                                            message: "Accepts a maximum of 60 characters"
-                                        }
-                                    })}
                                     placeholder="Last name"
                                     className="text-base font-medium focus:outline-none ring-2 ring-gray-400 p-2 rounded-md focus:ring-2 focus:ring-dark-blue"
                                 />
-                                 <ErrorMessage
-                                   errors={errors}
-                                   name="last_name"
-                                   render={({ messages }) =>
-                                     messages &&
-                                     Object.entries(messages).map(([type, message]) => (
-                                       <p key={type} className="text-sm text-red-500 font-normal">{message}</p>
-                                     ))
-                                   }
-                                 />
+                                 
                             </div>
                         </div>
                         <div className="flex flex-col gap-y-3">
@@ -110,83 +51,32 @@ export default function ContactForm() {
                                 <label htmlFor="email-address">Email</label>
                                 <input 
                                     type="text" 
-                                    {...register("email_address", {
-                                        required: {
-                                            value: true,
-                                            message: "Email Address is required"
-                                        },
-                                      
-                                    })}
+                                    
                                     placeholder="you@email.com"
                                     className="text-base font-medium focus:outline-none ring-2 ring-gray-400 p-2 rounded-md focus:ring-2 focus:ring-dark-blue"
                                 />
-                                 <ErrorMessage
-                                   errors={errors}
-                                   name="email_address"
-                                   render={({ messages }) =>
-                                     messages &&
-                                     Object.entries(messages).map(([type, message]) => (
-                                       <p key={type} className="text-sm text-red-500 font-normal">{message}</p>
-                                     ))
-                                   }
-                                 />
+                                 
                             </div>
                             <div className="flex flex-col gap-y-2">
                                
                                 <label htmlFor="phone-number">Phone Number</label>
                                 <input 
                                     type="text"
-                                    {...register("phone_number", {
-                                        required: {
-                                            value: true,
-                                            message: "Phone number is required"
-                                        },
-                                        maxLength: {
-                                            value: 15,
-                                            message: "Accepts a maximum of 15 characters"
-                                        }
-                                    })}
+                                   
                                     placeholder="+233 00 000 0000"
                                     className="text-base font-medium focus:outline-none ring-2 ring-gray-400 p-2 rounded-md focus:ring-2 focus:ring-dark-blue"
                                 />
-                                 <ErrorMessage
-                                   errors={errors}
-                                   name="phone_number"
-                                   render={({ messages }) =>
-                                     messages &&
-                                     Object.entries(messages).map(([type, message]) => (
-                                       <p key={type} className="text-sm text-red-500 font-normal">{message}</p>
-                                     ))
-                                   }
-                                 />
-                            
                             </div>
                             <div className="flex flex-col gap-y-2">
                                 <label htmlFor="msg">Message</label>
                               
                                <textarea 
                                     cols={25} rows={7} maxLength={300}
-                                    {...register("message", {
-                                        required: {
-                                            value: true,
-                                            message: "First name is required"
-                                        },
-                                      
-                                    })} 
                                     className="text-base font-medium focus:outline-none ring-2 ring-gray-400 p-2 rounded-md focus:ring-2 focus:ring-dark-blue"
                                 />
-                                 <ErrorMessage
-                                   errors={errors}
-                                   name="message"
-                                   render={({ messages }) =>
-                                     messages &&
-                                     Object.entries(messages).map(([type, message]) => (
-                                       <p key={type} className="text-sm text-red-500 font-normal">{message}</p>
-                                     ))
-                                   }
-                                 />
+                                
                             </div>
-                            <div className="flex flex-col gap-y-2 mt-2">
+                            <div className="flex flex-col gap-y-2 mt-2 mb-5">
                                 <button type="submit" className="bg-dark-blue text-lg !font-medium  text-white p-2 rounded-md hover:bg-opacity-90 transition-transform duration-300 ease-in-out hover:translate-y-0 hover:scale-95 delay-100">
                                     Send message
                                 </button>
@@ -196,7 +86,7 @@ export default function ContactForm() {
                 </div>
                 <div className=" flex flex-row ">
                     <div className="">
-                       <Image 
+                       {/* <Image 
                             src={img_one}
                             alt="coffee cup and jug"
                             width={650}
@@ -204,8 +94,10 @@ export default function ContactForm() {
                             className="!p-0 !m-0 h-screen"
                             sizes="(max-with: 100%) 100vh"
                             // style={{objectPosition: 'right', objectFit: 'cover'}}
-                       />
-                       
+                       /> */}
+
+                       {/* You can  use either the carousel or the single image */}
+                       <CarouselComponent /> 
                     </div>
                 </div>
             </div>
